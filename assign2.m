f59c549a3e7e3af4976f8ae1e9e0f110a8070e6b
anupriyalathey <anupriyalathey@gmail.com>
2024-01-25 10:19:08 +0530

    update assign 2

--- assign2.m
+++ assign2.m
@@ -22,11 +22,11 @@ x23 = (B(3) - A(3,1).*x1)./A(3,2);
 % Default increment = 1
 
 % Compare each entry in x21 such that negative values are substituted by 0
-% semicolon at end doesn't show up in output
+% semicolon at end doesnt show up in output
 
-x21 = max(0,x21)
-x22 = max(0,x22)
-x23 = max(0,x23)
+x21 = max(0,x21);
+x22 = max(0,x22);
+x23 = max(0,x23);
 
 % red, blue, black
 % everything sequence wise
@@ -36,3 +36,48 @@ title('x1 vs x2')
 xlabel('Value of x1')
 ylabel('Value of x2')
 legend('x1+2x2=10', 'x1+x2=6', 'x1-2x2=1')
+
+% Phase 3: Finding corner points with axes
+% Output gives the indexes in x1, x21, x22, x23 where the values are 0
+% More than 2 points occur since we fixed thw negative values to 0 previously
+
+cx1 = find(x1==0);
+c1 = find(x21==0);
+c2 = find(x22==0);
+c3 = find(x23==0);
+
+% Transpose
+Line1 = ([x1([c1 cx1]); x21([c1 cx1])])';
+Line2 = ([x1([c2 cx1]); x22([c2 cx1])])';
+Line3 = ([x1([c3 cx1]); x23([c3 cx1])])';
+
+% 'Since want unique in row-wise sense
+% [5 0 -> Different; [5 0 -> Same
+%  0 5]               5 0]
+% Corner Points
+
+corpt = unique([Line1; Line2; Line3], 'rows')
+
+% Phase 4: Intersecting points of each line
+
+% Denotes Origin(0,0) in column form
+pt = [0;0]
+% size(A): 3x2; size(A,1): no. of rows; size(A,2): no. of columns
+% no. of constraints
+for i = 1:size(A,1)
+    for j = i+1:size(A,1)
+    A1=A([i,j], :)
+    B1=B([i,j], :)
+    x = inv(A1)*B1
+    pt = [pt x]
+    end
+end
+
+intersecting_pt = pt';
+
+% Phase 5: All corner points
+% Not all corner points are feasible'
+all_corpt = unique([intersecting_pt; corpt], 'rows')
+
+
+
